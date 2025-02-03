2cd8096f76838dfdbdbf8b04faa432af19d1dd3a
AWSminhduc <awsminhduchoang@gmail.com>
2025-02-02 23:03:37 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ca-central-1_KUT30n7iQ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '14aj4n8pc88l1agi9l9a0692ce', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3mblbonp6a83ev5ucpiss7vjn3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
