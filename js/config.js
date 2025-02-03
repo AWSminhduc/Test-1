d8d768ea60c60f4e2d69e891e1eed4fc750cc302
AWSminhduc <awsminhduchoang@gmail.com>
2025-02-02 22:10:02 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ca-central-1_KUT30n7iQ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '14aj4n8pc88l1agi9l9a0692ce', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
